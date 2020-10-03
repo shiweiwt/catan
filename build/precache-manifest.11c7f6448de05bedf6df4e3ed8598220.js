d00fc044c60cefdc26a35c4201cfc968944350c2
Shiwei Wang <shiweiwtest@gmail.com>
2020-10-03 11:41:37 -0700

    Rebuild

---
+++ build/precache-manifest.11c7f6448de05bedf6df4e3ed8598220.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "061763a73b8c7ee9cbe635cc5ef19d9d",
+    "url": "/catan/index.html"
+  },
+  {
+    "revision": "79c6e5316316fd1ed29e",
+    "url": "/catan/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a4658c60e55d22a636c1",
+    "url": "/catan/static/css/main.2a2c946e.chunk.css"
+  },
+  {
+    "revision": "79c6e5316316fd1ed29e",
+    "url": "/catan/static/js/2.e54925b4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/catan/static/js/2.e54925b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4658c60e55d22a636c1",
+    "url": "/catan/static/js/main.698cc1a9.chunk.js"
+  },
+  {
+    "revision": "3a2f1e43c03a25d0b114",
+    "url": "/catan/static/js/runtime-main.0b02ec9a.js"
+  },
+  {
+    "revision": "c9ce5fbf15f29f65a043ab370066c4b6",
+    "url": "/catan/static/media/brick.c9ce5fbf.png"
+  },
+  {
+    "revision": "ac87c7711c5ba6c9d0de9814c9ec34c7",
+    "url": "/catan/static/media/desert.ac87c771.png"
+  },
+  {
+    "revision": "dc5ae611486a7de08c4e22eebf764e3a",
+    "url": "/catan/static/media/ore.dc5ae611.png"
+  },
+  {
+    "revision": "dcc88d02b977cafd3a5ad06ba22e3959",
+    "url": "/catan/static/media/sheep.dcc88d02.png"
+  },
+  {
+    "revision": "54206543e22a7c915dc06d2f1b779282",
+    "url": "/catan/static/media/wheat.54206543.png"
+  },
+  {
+    "revision": "09af1d5a845904b1e44af1d12e4d829f",
+    "url": "/catan/static/media/wood.09af1d5a.png"
+  }
+]);
